4c39f04ac37a673c148b321883018d449f591767
hara-yuto <hara_yyy0421@outlook.jp>
2021-03-22 17:58:28 +0900

    テストコード入力

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+
+
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category , :condition , :payer , :area , :delivery_day
   belongs_to :user
@@ -6,13 +9,18 @@ class Item < ApplicationRecord
 
   with_options presence: true do
   validates :image
-  validates :name
-  validates :introduction
-  validates :category_id          ,numericality: { other_than: 1, message:"Select"}
-  validates :condition_id         ,numericality: { other_than: 1,message:"Select"}
-  validates :payer_id             ,numericality: { other_than: 1, message:"Select"}
-  validates :area_id              ,numericality: { other_than: 1, message:"Select"}
-  validates :delivery_day_id      ,numericality: { other_than: 1 ,message:"Select"}
+  validates :name,            length:{maximum: 40}
+  validates :introduction,    length:{maximum: 1000}
+  
+
+  with_options numericality: { other_than: 1, message:"Select"} do
+  validates :category_id          
+  validates :condition_id         
+  validates :payer_id             
+  validates :area_id              
+  validates :delivery_day_id     
+  end
+
   validates :price,      format: { with: /\A[0-9]+\z/},
   numericality: { only_integer: true,greater_than: 299, less_than: 10000000, message:"Out of setting range"}, allow_blank: true
   validates :price
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name         {Faker::Name.name}
+    introduction {Faker::Lorem.sentence}
+    category_id {2}
+    condition_id {2}
+    payer_id {2}
+    area_id {2}
+    delivery_day_id {2}
+    price            {'300'}
+    user_id {2}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
