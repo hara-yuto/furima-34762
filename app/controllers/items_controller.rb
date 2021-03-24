2f87a4a94c1a707cf9606605bf09232e1464a093
hara-yuto <hara_yyy0421@outlook.jp>
2021-03-24 16:01:32 +0900

    【遷移】ルーティング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!  , except: [:index,:show]
-  before_action :set_item, only: [:show,:edit,:update]
-  before_action :syori_item,only: [:edit,:update]
+  before_action :set_item, only: [:show,:edit,:update,:destroy]
+  before_action :syori_item,only: [:edit,:update,:destroy]
 
   def index
     @items =Item.order(created_at: :desc)
@@ -37,6 +37,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
 
 
