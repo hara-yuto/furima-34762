2bc3ad11704f5e1d758791548f83f429b32a3276
hara-yuto <hara_yyy0421@outlook.jp>
2021-03-26 14:37:22 +0900

    購入画面　情報入力反映

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -9,7 +9,14 @@ class BuyersController < ApplicationController
   end
 
   def create
-
+    @address=Address.new(address_params)
+    redirect_to root_path
   end
 
+  private
+
+
+  def address_params
+    params.permit(:post_code, :area_id, :city, :house_number, :house_name,:phone_number).merge(user_id: current_user.id)
+  end
 end
