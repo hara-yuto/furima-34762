3c15a332238c2b8d723c4ad7c8799e96ef25c80e
hara-yuto <hara_yyy0421@outlook.jp>
2021-03-26 12:00:46 +0900

    【設定】ルーティング

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -3,4 +3,13 @@ class BuyersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-  resources :buyers,only: :index
+  resources :buyers,only: [:index,:new,:create]
 
   end
 end
