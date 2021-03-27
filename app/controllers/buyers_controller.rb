916cfdf492d36849a21e6fdc3f00265d462f5db6
hara-yuto <hara_yyy0421@outlook.jp>
2021-03-27 21:08:04 +0900

    リファクタリング

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -15,12 +15,7 @@ class BuyersController < ApplicationController
     @item = Item.find(params[:item_id])
     @credit_address=CreditAddress.new(buyer_params)
  if @credit_address.valid?
-  Payjp.api_key = "sk_test_570e68d8d6bfb1ae3d3bbb52"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-  Payjp::Charge.create(
-    amount: @item.price,
-    card: buyer_params[:token],
-    currency: 'jpy'  
-  )
+    pay_item
     @credit_address.save
     redirect_to root_path
   else
@@ -34,6 +29,17 @@ class BuyersController < ApplicationController
   def buyer_params
     params.require(:credit_address).permit(:post_code, :area_id, :city, :house_number, :house_name,:phone_number,:user_id,:item_id).merge(user_id: current_user.id,token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = "sk_test_570e68d8d6bfb1ae3d3bbb52"  
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buyer_params[:token],
+      currency: 'jpy'  
+    )
+
+  end
+
 
 
 end
