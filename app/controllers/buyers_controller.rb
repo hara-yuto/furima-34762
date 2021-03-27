de179f253a17dc6e56d8fc9ff7c692d1f4778578
hara-yuto <hara_yyy0421@outlook.jp>
2021-03-27 17:41:07 +0900

    エラーメッセージ反映

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -2,15 +2,13 @@ class BuyersController < ApplicationController
  
   def index
     @item = Item.find(params[:item_id])
-   # @credit_address=CreditAddress.new(buyer_params)
-    @buyer=Buyer.new(buyer_params)
+    @credit_address=CreditAddress.new
+    #@credit_address=CreditAddress.new(buyer_params)
+    #@buyer=Buyer.new(buyer_params)
   end
-  
-  def new
-  
-  end
-
+ 
   def create
+    @item = Item.find(params[:item_id])
     @credit_address=CreditAddress.new(buyer_params)
  if @credit_address.valid?
     @credit_address.save
@@ -24,6 +22,6 @@ class BuyersController < ApplicationController
 
 
   def buyer_params
-    params.permit(:post_code, :area_id, :city, :house_number, :house_name,:phone_number,:user_id,:item_id).merge(user_id: current_user.id)
+    params.require(:credit_address).permit(:post_code, :area_id, :city, :house_number, :house_name,:phone_number,:user_id,:item_id).merge(user_id: current_user.id)
   end
 end
