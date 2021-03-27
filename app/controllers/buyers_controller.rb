ea8ab5692356d243c96a1dd4bd43e4e60c6c89fe
hara-yuto <hara_yyy0421@outlook.jp>
2021-03-27 18:36:54 +0900

    【トップ遷移】出品者が自分の購入ページに直接行った場合

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -5,7 +5,9 @@ class BuyersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @credit_address=CreditAddress.new
-
+      if current_user == @item.user
+      redirect_to root_path
+      end
   end
  
   def create
@@ -25,7 +27,6 @@ class BuyersController < ApplicationController
   def buyer_params
     params.require(:credit_address).permit(:post_code, :area_id, :city, :house_number, :house_name,:phone_number,:user_id,:item_id).merge(user_id: current_user.id)
   end
-
 
 
 end
