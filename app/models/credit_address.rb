3e0d08896dff8b8459847d1ee8696d36c1beb21a
hara-yuto <hara_yyy0421@outlook.jp>
2021-03-28 10:57:41 +0900

    エラーメッセージの重複制限

--- app/models/credit_address.rb
+++ app/models/credit_address.rb
@@ -4,13 +4,16 @@ class CreditAddress
 
    with_options presence: true do
      validates :token 
-     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}, allow_blank: true
+     validates :post_code
+     validates :area_id, numericality: {other_than: 1, message: "can't be blank"}
+     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}, allow_blank: true
+     validates :city
      validates :house_number
-     validates :phone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."} 
+     validates :phone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."} , allow_blank: true
+     validates :phone_number
    end
-   validates :area_id, numericality: {other_than: 1, message: "can't be blank"}
-
+     
   def save
        
        buyer = Buyer.create(item_id: item_id, user_id: user_id)
