5a1e1081f690af628342e8e773c5d4c5448c6462
hara-yuto <hara_yyy0421@outlook.jp>
2021-03-30 11:27:06 +0900

    【修正１】編集ページ、削除ページ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,6 +50,6 @@ class ItemsController < ApplicationController
   end
 
   def syori_item_buy
-    redirect_to root_path if current_user.id == @item.user.id || @item.buyer.present?
+    redirect_to root_path if current_user.id != @item.user.id || @item.buyer.present?
   end
 end
